0d69c0ff2704149d9355ff828f551027005fcaf1
eduard.hroza <eduard.hroza@gmail.com>
2023-11-19 18:41:21 -0800

    Добавлены страницы about и about/author

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module TestGuru
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
+    config.time_zone = 'America/Vancouver'
+    config.i18n.default_locale = :en
 
     # Configuration for the application, engines, and railties goes here.
     #
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  get 'static_pages/about'
+  get 'static_pages/author'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
+  get '/about', to: 'static_pages#about'
+  get '/about/author', to: 'static_pages#author'
   # Defines the root path route ("/")
   # root "posts#index"
 end
